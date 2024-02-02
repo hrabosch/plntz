4bf563fc47fbcdfcd29f13ac46c2ef7d692fa31c
Aleš Laňar <ales.lanar@gmail.com>
2024-02-02 20:41:41 +0100

    Prep scrollable bottom menu with items

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:plntz/model/layout_object.dart';
@@ -22,6 +23,7 @@ class GardenLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     List<LayoutObject> layoutObjects = [];
+    List<String> placeableItems = ["Item1", "Item2", "Item3"];
     return Scaffold(
       body: Center(
         child: Column(
@@ -57,9 +59,19 @@ class GardenLayout extends StatelessWidget {
               Expanded(
                   flex: 2,
                   child: Container(
-                    color: Colors.brown,
-                    child: const Text("Menu with placable items"),
-                  ))
+                      color: Colors.brown,
+                      child: ListView.builder(
+                        itemCount: 10,
+                        physics: const ClampingScrollPhysics(),
+                        scrollDirection: Axis.horizontal,
+                        shrinkWrap: true,
+                        itemBuilder: (BuildContext context, int index) {
+                          return Container(
+                            width: 100,
+                            child: const Card(elevation: 10, child: Icon(Icons.bakery_dining)),
+                          );
+                        },
+                      )))
             ]),
       ),
     );
